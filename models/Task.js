5f477f40a12707572437158654e277e2c9b3b5a1
Maximiliano Pereyra <maximilianop98@gmail.com>
2024-12-27 13:13:44 -0300

    Remove node_modules and add to .gitignore

--- models/Task.js
+++ models/Task.js
@@ -5,7 +5,12 @@ const taskSchema = new mongoose.Schema({
   pedido: { type: String, required: true },
   descripcion: { type: String, required: true },
   prioridad: { type: String, required: true },
+<<<<<<< HEAD
   fechaCarga: { type: Date, default: Date.now }
+=======
+  fechaCarga: { type: Date, default: Date.now },
+  completed: { type: Boolean, default: false } 
+>>>>>>> 6e118b4 (Actualiza la estructura de archivos del proyecto)
 });
 
 module.exports = mongoose.model('Task', taskSchema);
