e3c43661987efa3e4ecc310a461f933480e424a4
Maximiliano Pereyra <maximilianop98@gmail.com>
2024-12-30 18:05:40 -0300

    Configurar CORS para permitir solicitudes desde Netlify

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const taskRoutes = require('./routes/taskRoutes');
-const authRoutes = require('./routes/authRoutes'); // Importa las rutas de autenticación
+const authRoutes = require('./routes/authRoutes');
 
-dotenv.config(); // Cargar variables de entorno desde .env
+dotenv.config();
 
 const app = express();
 
@@ -14,24 +14,34 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
-  console.log('Conectado a la base de datos'); // Mensaje de depuración
+  console.log('Conectado a la base de datos');
 }).catch(err => {
-  console.error('Error al conectar a la base de datos:', err); // Mensaje de depuración
+  console.error('Error al conectar a la base de datos:', err);
 });
 
 // Configurar CORS
 const corsOptions = {
   origin: 'https://saynomoremantenimiento.netlify.app', // Cambia esto a la URL de tu frontend en Netlify
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  credentials: true
 };
 
-app.use(cors(corsOptions)); // Habilitar CORS para todas las rutas
+app.use(cors(corsOptions));
 app.use(express.json());
 
+// Middleware de depuración
+app.use((req, res, next) => {
+  console.log('Solicitud recibida:', req.method, req.url);
+  next();
+});
+
+// Rutas
 app.use('/api/tasks', taskRoutes);
-app.use('/api/auth', authRoutes); // Usa las rutas de autenticación
+app.use('/api/auth', cors(corsOptions), authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`); // Mensaje de depuración
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
