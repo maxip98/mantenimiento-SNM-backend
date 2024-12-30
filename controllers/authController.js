6e38d68f78b6138ee813dc9cc5e61b37032c1c40
Maximiliano Pereyra <maximilianop98@gmail.com>
2024-12-30 18:50:53 -0300

    Configurar CORS para permitir solicitudes desde Netlify

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,27 +4,37 @@ const User = require('../models/User');
 exports.register = async (req, res) => {
   const { username, password, role } = req.body;
   console.log('Registrando usuario:', username); // Mensaje de depuración
-  const user = new User({ username, password, role });
-  await user.save();
-  res.status(201).send('Usuario registrado');
+  try {
+    const user = new User({ username, password, role });
+    await user.save();
+    res.status(201).send('Usuario registrado');
+  } catch (error) {
+    console.error('Error al registrar usuario:', error);
+    res.status(500).send('Error al registrar usuario');
+  }
 };
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   console.log('Intentando iniciar sesión con:', username); // Mensaje de depuración
-  const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
 
-  if (!user) {
-    console.log('Usuario no encontrado'); // Mensaje de depuración
-    return res.status(401).send('Credenciales incorrectas');
+    if (!user) {
+      console.log('Usuario no encontrado'); // Mensaje de depuración
+      return res.status(401).send('Credenciales incorrectas');
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      console.log('Contraseña incorrecta'); // Mensaje de depuración
+      return res.status(401).send('Credenciales incorrectas');
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    console.error('Error durante el inicio de sesión:', error);
+    res.status(500).send('Error durante el inicio de sesión');
   }
-
-  const isMatch = await user.comparePassword(password);
-  if (!isMatch) {
-    console.log('Contraseña incorrecta'); // Mensaje de depuración
-    return res.status(401).send('Credenciales incorrectas');
-  }
-
-  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
 };
