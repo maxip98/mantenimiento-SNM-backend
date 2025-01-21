a916a3fc57eeb930e3b3e8607fd7808638b24b20
Maximiliano Pereyra <maximilianop98@gmail.com>
2025-01-21 13:32:19 -0300

    Task y AuthRoutes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,3 +1,6 @@
+// Define las rutas para la gestión de tareas, 
+// incluyendo la obtención, creación, actualización, eliminación y marcación de tareas como completadas, con verificación de roles.
+
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
@@ -5,7 +8,7 @@ const { verifyRole } = require('../middleware/authMiddleware');
 
 router.get('/', verifyRole(['admin', 'viewer']), taskController.getTasks);
 router.post('/', verifyRole(['admin']), taskController.createTask);
-router.put('/:id', verifyRole(['admin']), taskController.updateTask);
+router.put('/:id', verifyRole(['admin', 'viewer']), taskController.updateTask); // Permitir que admin y viewer actualicen tareas
 router.delete('/:id', verifyRole(['admin']), taskController.deleteTask);
 router.put('/:id/complete', verifyRole(['admin']), taskController.completeTask);
 
